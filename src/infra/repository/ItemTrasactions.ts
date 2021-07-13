da5d8f869e2c7186a80d551971ec6f36d2b96bca
victorfernandesraton <vfbraton@gmail.com>
2021-07-13 17:34:40 -0300

    implement createLoan

--- src/infra/repository/ItemTrasactions.ts
+++
@@ -1,7 +0,0 @@
-import Item from '../../core/entity/Item';
-import Transaction from '../../core/entity/Transaction';
-
-export default interface ItemTrasactionsRepository {
-  createTransaction(transaction: Transaction): Promise<Transaction>;
-  lastTrasactionState(item: Item): Promise<Transaction | null>;
-}
---
+++ src/usecase/CreateLoan.ts
@@ -0,0 +1,67 @@
+import Transaction, { TransactionEnum } from '../core/entity/Transaction';
+import ItemTrasactionsRepository from '../infra/repository/ItemTrasactionsRepository';
+import ItemRepository from '../infra/repository/ItemRepository';
+import UserRepository from '../infra/repository/UserRepository';
+
+export interface CreateLoanParams {
+  itemRepository: ItemRepository;
+  itemTrasactionsRepository: ItemTrasactionsRepository;
+  userRepository: UserRepository;
+}
+
+export interface CreateLoanExecuteParams {
+  itemId: string;
+  userId: string;
+  status: TransactionEnum;
+}
+
+export default class CreateLoan {
+  itemRepository: ItemRepository;
+  ItemTrasactionsRepository: ItemTrasactionsRepository;
+  UserRepository: UserRepository;
+  constructor({
+    itemRepository,
+    itemTrasactionsRepository,
+    userRepository,
+  }: CreateLoanParams) {
+    this.ItemTrasactionsRepository = itemTrasactionsRepository;
+    this.UserRepository = userRepository;
+    this.itemRepository = itemRepository;
+  }
+
+  async execute({
+    itemId,
+    userId,
+    status,
+  }: CreateLoanExecuteParams): Promise<Transaction> {
+    const [item, user, lastItemTransaction] = await Promise.all([
+      this.itemRepository.find(itemId),
+      this.UserRepository.find(userId),
+      this.ItemTrasactionsRepository.lastTrasaction(itemId),
+    ]);
+
+    if (!item) {
+      throw new Error(`Item (${itemId} is not found`);
+    }
+
+    if (!user) {
+      throw new Error(`User (${userId} is not found`);
+    }
+
+    if (
+      [TransactionEnum.Canceled, TransactionEnum.Loan].includes(
+        lastItemTransaction?.status
+      )
+    ) {
+      throw new Error(
+        `Cannot execute transaction because item (${itemId} is been unvaliable`
+      );
+    }
+
+    return await this.ItemTrasactionsRepository.createTransaction(
+      item,
+      status,
+      user
+    );
+  }
+}
