8e0fd4d9ed1810cad5c17836e4b61981b40f8537
victorfernandesraton <vfbraton@gmail.com>
2021-09-22 00:53:38 -0300

    update docker config

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM node:14-alpine
 
 WORKDIR /app
-COPY ["package.json", "package-lock.json*", "./"]
+RUN node --version
+RUN npm --version
+COPY ["package.json", "./"]
 RUN npm install
 COPY . .
+
+EXPOSE 8000
 CMD ["npm", "start"]
--- src/app/http/index.ts
+++ src/app/http/index.ts
@@ -9,25 +9,32 @@ import StorageCreateController from './controller/StorageCreateController';
 import MongoDBFactory from '../../infra/repository/mongodb/Factory';
 import { BaseExpressController } from './Express/Controller';
 const start = async () => {
-  const uri = 'mongodb://localhost:27017/?readPreference=primary&ssl=false';
+  const uri = process.env.MONGODB_HOST;
 
-  await MongoHelper.connect(uri);
-  const router = Router();
-  const controller = new BaseExpressController();
-  const { storage } = await MongoDBFactory(MongoHelper);
-  const createStorage = new CreateStorage({ repository: storage });
-  const storageCreateController = new StorageCreateController(
-    createStorage,
-    controller
-  );
+  try {
+    await MongoHelper.connect(uri);
+    const router = Router();
+    const controller = new BaseExpressController();
+    const { storage } = await MongoDBFactory(MongoHelper);
+    const createStorage = new CreateStorage({ repository: storage });
+    const storageCreateController = new StorageCreateController(
+      createStorage,
+      controller
+    );
 
-  const storageRoute = StorageRoute(storageCreateController, router);
-  const route = Router();
+    const storageRoute = StorageRoute(storageCreateController, router);
+    const route = Router();
 
-  route.use('/storage', storageRoute);
+    route.use('/storage', storageRoute);
 
-  const Server = createServer(Application(route));
-  Server.listen(8000);
+    const Server = createServer(Application(route));
+    Server.listen(process.env.PORT);
+    Server.on('listening', () => {
+      console.info('Server is runing...');
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 start();
